76724a41778d5d07617837cc82885659094dcf25
MahdiDev78 <MahdiDev78@gmail.com>
2021-09-29 15:39:52 +0330

    create new animators

--- animateddialog/src/main/java/com/mahdikh/vision/core/app/animateddialog/animator/FoldXAnimator.kt
+++ animateddialog/src/main/java/com/mahdikh/vision/core/app/animateddialog/animator/FoldXAnimator.kt
@@ -3,27 +3,27 @@ package com.mahdikh.vision.core.app.animateddialog.animator
 import android.view.View
 import android.view.ViewPropertyAnimator
 
-class FoldAnimator : Animator() {
+class FoldXAnimator : Animator() {
     override fun animate(animator: ViewPropertyAnimator) {
         if (mode == MODE_IN) {
             animator
                 .alpha(1.0F)
-                .scaleY(1.0F)
+                .scaleX(1.0F)
         } else {
             animator
                 .alpha(0.2F)
-                .scaleY(0.0F)
-                .scaleX(1.3F)
+                .scaleX(0.0F)
+                .scaleY(1.3F)
         }
     }
 
     override fun onBeforeAnimate(view: View) {
         if (mode == MODE_IN) {
             view.alpha = 0.5F
-            view.scaleY = 0.0F
+            view.scaleX = 0.0F
         } else {
             view.alpha = 1.0F
-            view.scaleY = 1.0F
+            view.scaleX = 1.0F
         }
     }
 }
